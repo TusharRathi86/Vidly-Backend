e9dca8e8c72ec8ff4fe491c9e50383b7eb2778d7
TusharRathi86 <tushar.rathi860@gmail.com>
2023-02-19 00:25:43 +0530

    Main Commit

--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,17 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
-// require("dotenv").config();
-const config = require("config");
+require("dotenv").config();
+// const config = require("config");
 
 module.exports = () => {
-  const db = config.get("db");
-  // const db = process.env.MONGO_URL;
-  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+  // const db = config.get("db");
+  const db = process.env.MONGO_URL;
+  mongoose
+    .connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => winston.info(`Connected to ${db}...`));
 };
